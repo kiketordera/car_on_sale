b350414fdc8cba0d5a16bb7f29228bd3afa64cde
Kike Tordera <dojranovski@gmail.com>
2024-05-24 11:43:25 +0200

    Step 5
    Display auction data & Navigation

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:car_on_sale/car_options.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
@@ -84,18 +85,20 @@ class HomeScreenState extends State<HomeScreen> {
 
   void _handleMultipleOptions(String responseBody) {
     List<VehicleOption> options = parseVehicleOptions(responseBody);
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => VehicleSelectionScreen(options),
-      ),
-    ).then((selectedVehicleId) {
-      if (selectedVehicleId != null) {
-        setState(() {
-          _statusMessage = 'Selected vehicle ID: $selectedVehicleId';
-        });
-      }
-    });
+    if (mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => VehicleSelectionScreen(options),
+        ),
+      ).then((selectedVehicleId) {
+        if (selectedVehicleId != null) {
+          setState(() {
+            _statusMessage = 'Selected vehicle ID: $selectedVehicleId';
+          });
+        }
+      });
+    }
   }
 
   void _handleError(String? reasonPhrase) {
@@ -153,39 +156,6 @@ class HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class VehicleOption {
-  final String make;
-  final String model;
-  final String containerName;
-  final int similarity;
-  final String externalId;
-
-  VehicleOption({
-    required this.make,
-    required this.model,
-    required this.containerName,
-    required this.similarity,
-    required this.externalId,
-  });
-
-  factory VehicleOption.fromJson(Map<String, dynamic> json) {
-    return VehicleOption(
-      make: json['make'],
-      model: json['model'],
-      containerName: json['containerName'],
-      similarity: json['similarity'],
-      externalId: json['externalId'],
-    );
-  }
-}
-
-List<VehicleOption> parseVehicleOptions(String responseBody) {
-  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-  return parsed
-      .map<VehicleOption>((json) => VehicleOption.fromJson(json))
-      .toList();
-}
-
 class VehicleSelectionScreen extends StatelessWidget {
   final List<VehicleOption> options;
 
@@ -236,14 +206,14 @@ class AuctionDataScreen extends StatelessWidget {
                 style: const TextStyle(fontSize: 18)),
             Text("Model: ${auctionData['model']}",
                 style: const TextStyle(fontSize: 18)),
-            Text("UUID: ${auctionData['uuid']}",
+            Text("UUID: ${auctionData['_fk_uuid_auction']}",
                 style: const TextStyle(fontSize: 18)),
             if (auctionData['feedback'] != null)
               Text(
-                "Feedback: ${auctionData['feedback']}",
+                "${auctionData['positiveCustomerFeedback'] ? "Positive" : "Negative"} Feedback: ${auctionData['feedback']}",
                 style: TextStyle(
                   fontSize: 18,
-                  color: auctionData['feedback'] == 'positive'
+                  color: auctionData['positiveCustomerFeedback']
                       ? Colors.green
                       : Colors.red,
                 ),
